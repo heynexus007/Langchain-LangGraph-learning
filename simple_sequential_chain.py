1ca5bc974ef838510bda26303dac7e16b6c25801
Abhishek Sahani <abthegamer007@gmail.com>
2026-03-31 11:26:45 +0000

    [Abhishek] added Implementation of SimpleSequentialChain

---
+++ simple_sequential_chain.py
@@ -0,0 +1,34 @@
+import os
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+import streamlit as st
+from langchain_core.output_parsers import StrOutputParser
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+title_prompt = PromptTemplate(input_variables=["topic"],
+                              template="""
+                              You are an experienced speech writer.
+                              You need to craft an impactful title for a speech on the following topics: {topic}
+                              Answers exactly with one title.
+                              """)
+
+speech_prompt = PromptTemplate(input_variables=["title"],
+                               template="""
+                               You need to write a professional speech of 350 words
+                               for the following title: {title}
+                               """)
+
+chain1 = title_prompt | llm | StrOutputParser()
+chain2 = speech_prompt | llm
+final_chain = chain1 | chain2
+
+st.title("Speech Generator")
+topic=st.text_input("Enter the topic here..")
+
+if st.button("Generate speech"):
+    st.write("Generating speech...")
+
+    if topic:
+        response = final_chain.invoke({"topic": topic})
+        st.write(response.content)
